e65755968f0587ea65bbde98cef547c3fac1cb20
Kuchen <daniel.schneeweis@a1.net>
2023-11-18 17:56:49 +0100

    added settings

---
+++ helper/settingHelper.go
@@ -0,0 +1,35 @@
+package helper
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	Retries int      `json:"retries"`
+	Timeout int      `json:"timeout"`
+	Hosts   []string `json:"hosts"`
+}
+
+var config Config
+
+func ReadSettings() {
+	data, err := ioutil.ReadFile("settings.json")
+	if err != nil {
+		fmt.Println("Error reading settings file:", err)
+		return
+	}
+
+	// Unmarshal the JSON data into the Config struct
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+}
+
+func getConfig() Config {
+	return config
+}
