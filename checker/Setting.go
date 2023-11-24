cf63de9d2f9e5925ecbbe9a1a66d0841a8746f78
Kuchen <daniel.schneeweis@a1.net>
2023-11-24 09:20:11 +0100

    added thread setting

--- checker/Setting.go
+++ checker/Setting.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Config struct {
+	Threads int      `json:"threads"`
 	Retries int      `json:"retries"`
 	Timeout int      `json:"timeout"`
 	Hosts   []string `json:"hosts"`
@@ -30,6 +31,6 @@ func ReadSettings() {
 
 }
 
-func getConfig() Config {
+func GetConfig() Config {
 	return config
 }
