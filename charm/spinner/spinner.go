09e75bed973dea7d4365a077955e992adde7116e
Kuchen <daniel.schneeweis@a1.net>
2023-11-26 22:30:05 +0100

    added spinner.go

---
+++ charm/spinner/spinner.go
@@ -0,0 +1,82 @@
+package spinner
+
+// A simple program demonstrating the spinner component from the Bubbles
+// component library.
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type errMsg error
+
+type model struct {
+	spinner  spinner.Model
+	quitting bool
+	err      error
+}
+
+var finished = false
+
+func initialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.Meter
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("#624CAB"))
+	return model{spinner: s}
+}
+
+func (m model) Init() tea.Cmd {
+	return m.spinner.Tick
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if finished {
+		return m, tea.Quit
+	}
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return m.err.Error()
+	}
+	str := fmt.Sprintf("\n\n   %s Loading hosts, please wait\n\n", m.spinner.View())
+	if m.quitting {
+		return str + "\n"
+	}
+	return str
+}
+
+func Run() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func SetFinished() {
+	finished = true
+}
