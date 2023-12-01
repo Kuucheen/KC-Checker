e8487c8a2d590e5a09ee8c698329b1d9e24c5aee
Kuchen <daniel.schneeweis@a1.net>
2023-12-01 14:47:31 +0100

    moved GetInput

--- helper/FileHelper.go
+++ helper/FileHelper.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Write(file string, data []byte) {
@@ -10,4 +11,14 @@ func Write(file string, data []byte) {
 	if err != nil {
 		fmt.Printf("Error while writing %s: %s", file, err)
 	}
+}
+
+func GetInput() []string {
+	dat, err := os.ReadFile("proxies.txt")
+	if err != nil {
+		fmt.Printf("Error while reading proxies: %s", err)
+	}
+
+	//Split into lines
+	return strings.Split(strings.ReplaceAll(string(dat), "\r\n", "\n"), "\n")
 }
