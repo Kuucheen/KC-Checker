3b49ad25b31d6dd32a0a86e52b066a9a278b08c0
Kuchen <daniel.schneeweis@a1.net>
2023-12-01 14:50:26 +0100

    added more process

--- charm/hostRunner.go
+++ charm/hostRunner.go
@@ -1,14 +1,34 @@
 package charm
 
 import (
-	"KC-Checker/charm/spinner"
+	"KC-Checker/charm/hosts"
+	"KC-Checker/charm/threadPhase"
 	"KC-Checker/common"
+	"KC-Checker/helper"
+	"sync"
+	"time"
 )
 
+var wg sync.WaitGroup
+
 func RunHostsDisplay() {
-	go spinner.Run()
-	hosts := common.CheckDomains()
-	spinner.SetFinished()
+	helper.SetType(RunType())
+
+	go hosts.Run()
+	hostsList := common.CheckDomains()
+	hosts.SetFinished()
 	DrawLogo()
-	DisplayHosts(hosts)
+	hosts.DisplayHosts(hostsList)
+	wg.Add(1)
+
+	go func() {
+		helper.Dispatcher(helper.ToProxies(helper.GetInput()))
+		defer wg.Done()
+	}()
+
+	time.Sleep(time.Second * 2)
+	threadPhase.RunBars()
+
+	wg.Wait()
+	threadPhase.SetFinished()
 }
