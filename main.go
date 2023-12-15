f0aa5ade4a6f880c6ab8e2baa716fd9a21deaeb5
Kuchen <daniel.schneeweis@a1.net>
2023-12-15 20:10:27 +0100

    updated main

--- main.go
+++ main.go
@@ -1,36 +1,17 @@
 package main
 
 import (
+	"KC-Checker/charm"
 	"KC-Checker/common"
-	"KC-Checker/helper"
-	"fmt"
 )
 
 func main() {
 	common.ReadSettings()
 
-	stringProxyArr := helper.GetInput()
-
-	if len(stringProxyArr) == 0 {
-		fmt.Print("Looks like you forgot to add some proxies to proxies.txt!")
-	}
-
-	proxyArr := helper.ToProxies(stringProxyArr)
-
-	helper.GetType()
-
-	fmt.Println("got type: ", helper.GetTypeName())
-
-	common.CheckDomains()
-
 	common.GetLocalIP()
 
-	helper.Dispatcher(proxyArr)
+	//hostsList := common.CheckDomains()
+	//hosts.DisplayHosts(hostsList)
 
-	fmt.Println("FINISHED!")
-
-	fmt.Println("Elite: ", len(helper.GetFinishedProxies()["elite"]))
-	fmt.Println("Anonymous: ", len(helper.GetFinishedProxies()["anonymous"]))
-	fmt.Println("Transparent: ", len(helper.GetFinishedProxies()["transparent"]))
-	fmt.Println("Invalid: ", helper.GetInvalid())
+	charm.RunHostsDisplay()
 }
