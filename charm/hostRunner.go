33902cf645473d044fd5d70aeafce60137b092f9
Kuchen <daniel.schneeweis@a1.net>
2023-12-15 22:19:32 +0100

    little changes

--- charm/hostRunner.go
+++ charm/hostRunner.go
@@ -1,31 +1,32 @@
 package charm
 
 import (
+	"KC-Checker/charm/hosts"
+	"KC-Checker/charm/threadPhase"
+	"KC-Checker/common"
 	"KC-Checker/helper"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
 
 func RunHostsDisplay() {
-	go DrawLogo()
+	DrawLogo()
 
 	helper.SetType(GetProxyType())
 
-	//go hosts.Run()
-	//hostsList := common.CheckDomains()
-	//hosts.SetFinished()
-	//DrawLogo()
-	//hosts.DisplayHosts(hostsList)
-	//wg.Add(1)
-	//
-	//go func() {
-	//	helper.Dispatcher(helper.ToProxies(helper.GetInput()))
-	//	defer wg.Done()
-	//}()
-	//
-	//time.Sleep(time.Second * 2)
-	//threadPhase.RunBars()
+	go common.CheckDomains()
+	hosts.Run()
+	wg.Add(1)
+
+	go func() {
+		helper.Dispatcher(helper.ToProxies(helper.GetInput("proxies.txt")))
+		defer wg.Done()
+	}()
+
+	time.Sleep(time.Second * 2)
+	threadPhase.RunBars()
 	//
 	//wg.Wait()
 	//threadPhase.SetFinished()
