4a278c46cafe6684781f6df708b009f6a56ba43a
Kuchen <daniel.schneeweis@a1.net>
2023-12-17 10:10:55 +0100

    renamed displayHosts.go

---
+++ charm/hosts/errorHolder.go
@@ -0,0 +1,57 @@
+package hosts
+
+import (
+	"KC-Checker/helper"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	style = lipgloss.NewStyle().Foreground(lipgloss.Color("#BE0101")).Width(125).Align(lipgloss.Center)
+	found = false
+)
+
+type waitingModel struct {
+	spinner spinner.Model
+}
+
+func (m waitingModel) Init() tea.Cmd {
+	return m.spinner.Tick
+}
+
+func (m waitingModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	helper.GetInput("proxies.txt")
+
+	if found {
+		time.Sleep(time.Second * 3)
+		return m, tea.Quit
+	} else if helper.ProxySum > 0 {
+		found = true
+	}
+
+	var cmd tea.Cmd
+	m.spinner, cmd = m.spinner.Update(msg)
+	return m, cmd
+}
+
+func (m waitingModel) View() string {
+	if found {
+		return style.Copy().Foreground(lipgloss.Color("#01BE85")).Render("Found " +
+			strconv.FormatFloat(helper.ProxySum, 'f', -1, 64) + " proxies")
+	}
+
+	return style.Render("It seems like you forgot to put proxies\nin proxies.txt\n"+
+		"You don't have to close this tab\n") + style.Copy().Foreground(lipgloss.Color("#FFF")).Render(
+		"\nIf you need proxies check out\nhttps://github.com/Kuucheen/KC-Scraper")
+}
+
+func WaitForProxies() {
+	w := waitingModel{}
+	if _, err := tea.NewProgram(w).Run(); err != nil {
+		os.Exit(1)
+	}
+}
