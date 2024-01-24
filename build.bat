b882ea5e334d4b59b7cfa490346047c7f6af816a
Kuchen <daniel.schneeweis@a1.net>
2024-01-24 09:14:38 +0100

    updated build so executeables have more info

--- build.bat
+++ build.bat
@@ -2,11 +2,14 @@
 
 set OUTPUT_FOLDER=bin
 set BINARY_NAME=KC-Checker
+set VERSION=1.0.4
+set DESCRIPTION=Open source proxy checker built in Go, fast and beautiful. Created by Kuchen (Kuucheen on GitHub).
 
 echo Compiling for Windows...
 set GOOS=windows
 set GOARCH=amd64
-go build -o %OUTPUT_FOLDER%\%BINARY_NAME%.exe
+go build -ldflags="-X 'main.Version=%VERSION%' -X 'main.Description=%DESCRIPTION%'" -o %OUTPUT_FOLDER%\%BINARY_NAME%.exe
+
 if %ERRORLEVEL% neq 0 (
     echo Compilation failed for Windows
     exit /b %ERRORLEVEL%
@@ -16,7 +19,7 @@ echo Compilation for Windows successful
 echo Compiling for Linux...
 set GOOS=linux
 set GOARCH=amd64
-go build -o %OUTPUT_FOLDER%\%BINARY_NAME%
+go build -ldflags="-X 'main.Version=%VERSION%' -X 'main.Description=%DESCRIPTION%'" -o %OUTPUT_FOLDER%\%BINARY_NAME%
 if %ERRORLEVEL% neq 0 (
     echo Compilation failed for Linux
     exit /b %ERRORLEVEL%
