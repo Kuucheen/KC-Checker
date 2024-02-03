c18ebac173ea3c75eefcc32cf0058c61cebedcd1
Kuchen <daniel.schneeweis@a1.net>
2024-02-03 23:05:22 +0100

    added judges_thread setting

--- common/Setting.go
+++ common/Setting.go
@@ -7,15 +7,16 @@ import (
 )
 
 type Config struct {
-	Threads     int      `json:"threads"`
-	Retries     int      `json:"retries"`
-	Timeout     int      `json:"timeout"`
-	PrivacyMode bool     `json:"privacy_mode"`
-	IpLookup    string   `json:"iplookup"`
-	Judges      []string `json:"judges"`
-	Blacklisted []string `json:"blacklisted"`
-	Bancheck    string   `json:"bancheck"`
-	Keywords    []string `json:"keywords"`
+	Threads       int      `json:"threads"`
+	Retries       int      `json:"retries"`
+	Timeout       int      `json:"timeout"`
+	PrivacyMode   bool     `json:"privacy_mode"`
+	IpLookup      string   `json:"iplookup"`
+	JudgesThreads int      `json:"judges_threads"`
+	Judges        []string `json:"judges"`
+	Blacklisted   []string `json:"blacklisted"`
+	Bancheck      string   `json:"bancheck"`
+	Keywords      []string `json:"keywords"`
 }
 
 var config Config
